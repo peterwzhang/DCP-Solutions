65df8424826a7e1425ed8bdcbaa9c16d01e0c6b8
Madeline Moore <mcmoore11@crimson.ua.edu>
2021-04-28 23:07:59 -0500

    removed echos

--- setup.sh
+++ setup.sh
@@ -7,11 +7,8 @@ notesFolder="Notes"
 for currTextFile in misc/textfiles/*
 do
   (
-  #echo "$currTextFile"
   file="${currTextFile%.txt}"
-  #echo "$file"
   file="${file##*/}"
-  #echo "$file"
   mkdir "$file"
   mv "$currTextFile" "$file/README.md"
   cd "$file" || exit
