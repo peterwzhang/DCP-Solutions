82a2ae1122952d6b251623be748b7e59ed36b4f2
Peter Zhang <peter.w.zhang@gmail.com>
2021-06-10 00:05:45 -0500

    add edge case handling to 49

--- solutions/049/Peter/main.cpp
+++ solutions/049/Peter/main.cpp
@@ -5,6 +5,7 @@ using namespace std;
 
 // Kadane's algorithm
 int solve_max(const vector<int> &array) {
+    if (array.size() == 0) return 0;
     int cur_max = array[0], max_sum = array[0];
     for (int i = 1; i < array.size(); i++) {
         cur_max += array[i];
@@ -13,10 +14,12 @@ int solve_max(const vector<int> &array) {
             cur_max = 0;
         }
     }
-    return max_sum;
+    return (max_sum < 0) ? 0 : max_sum;
 }
 
 int main(){
     vector<int> arr = {34, -50, 42, 14, -5, 86};
+    vector<int> arr2 = {-5, -1, -8, -9};
     cout << solve_max(arr) << endl;
+    cout << solve_max(arr2) << endl;
 }
