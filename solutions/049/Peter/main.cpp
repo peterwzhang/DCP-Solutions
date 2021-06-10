4f20364a0699818bd0e524cd05844eabd60f6427
Peter Zhang <peter.w.zhang@gmail.com>
2021-06-10 00:06:03 -0500

    formatting

--- solutions/049/Peter/main.cpp
+++ solutions/049/Peter/main.cpp
@@ -10,14 +10,14 @@ int solve_max(const vector<int> &array) {
     for (int i = 1; i < array.size(); i++) {
         cur_max += array[i];
         max_sum = max(cur_max, max_sum);
-        if (cur_max < 0){
+        if (cur_max < 0) {
             cur_max = 0;
         }
     }
     return (max_sum < 0) ? 0 : max_sum;
 }
 
-int main(){
+int main() {
     vector<int> arr = {34, -50, 42, 14, -5, 86};
     vector<int> arr2 = {-5, -1, -8, -9};
     cout << solve_max(arr) << endl;
